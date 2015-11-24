b56c2720a154b111ccc12464699d2996ad59e5ee
viveknallur <vivek.nallur@gmail.com>
2015-11-24 22:00:45 +0000

    Fixed typos in docker hub article

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+echo -e "\033[0;32mRemoving the public directory if it exists...\033[0m"
+rm -rf public/
 
+echo -e "\033[0;32mRemoving the git cache of public...\033[0m"
+git rm --cached public
+
+echo -e "\033[0;32mAdding public as a submodule...\033[0m"
+git submodule add https://github.com/viveknallur/viveknallur.github.io.git public 
+
+echo -e "\033[0;32mBuilding site...\033[0m"
 # Build the project.
 hugo # if using a theme, replace by `hugo -t <yourtheme>`
 
@@ -15,7 +23,11 @@ msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
+
+echo -e "\033[0;32mCommitting...\033[0m"
 git commit -m "$msg"
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Push source and build repos.
 git push origin master
