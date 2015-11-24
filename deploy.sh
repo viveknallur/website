fb16f39bac532d3ffd2796a0c6a3f33f3564da7f
viveknallur <vivek.nallur@gmail.com>
2015-11-24 22:03:43 +0000

    Fixed typos in docker hub article

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ echo -e "\033[0;32mRemoving the git cache of public...\033[0m"
 git rm --cached public
 
 echo -e "\033[0;32mAdding public as a submodule...\033[0m"
-git submodule add https://github.com/viveknallur/viveknallur.github.io.git public 
+git submodule add --force https://github.com/viveknallur/viveknallur.github.io.git public 
 
 echo -e "\033[0;32mBuilding site...\033[0m"
 # Build the project.
